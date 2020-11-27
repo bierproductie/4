564cf919ac35e7747137b718bf90b891ee1c1b26
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-11-27 08:17:35 +0100

    :green_heart: Replace tab with spaces

--- make/install.mk
+++ make/install.mk
@@ -1,16 +1,9 @@
 install: install-${TARGET}
 
 install-docker: ${VERSION} requirements.install
-	@docker-compose build
+    @docker-compose build
 
 install-local: ${VERSION} requirements.install
-	@pip install \
-		-e '.[dev]'
+	@pip install -e '.[dev]'
 
 install-ci: install-local
-
-install-production:
-	# TODO(Rathka: This should install inside a virtual env.)
-	# Assume that we are inside a venv.
-	# run scripts/generate_deployment_resources.sh might need venv arg (something like that)
-	# pip install .
